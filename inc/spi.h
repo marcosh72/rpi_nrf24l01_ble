beae2d4b2e04accb1d1d576537b80a394b8d720a
Luciano Silva do Lago <luciano.slago@gmail.com>
2016-11-18 17:02:03 -0200

    rasp hardware spi support added

--- inc/spi.h
+++ inc/spi.h
@@ -2,11 +2,12 @@
 #define SPI_H_
 
 #include <stdint.h>
+#include <bcm2835.h>
 
-#define SPI_MODE_0              0
-#define SPI_MODE_1              1
-#define SPI_MODE_2              2
-#define SPI_MODE_3              3
+#define SPI_MODE_0              BCM2835_SPI_MODE0
+#define SPI_MODE_1              BCM2835_SPI_MODE1
+#define SPI_MODE_2              BCM2835_SPI_MODE2
+#define SPI_MODE_3              BCM2835_SPI_MODE3
 
 class SPI{
 private:
@@ -15,6 +16,7 @@ private:
   uint32_t clock_frequency;
 public:
   SPI(uint32_t clk_frequency, uint_fast8_t mode);
+  ~SPI(void);
   static uint_fast8_t WriteRead(uint8_t *tx_data, uint8_t *rx_data, uint_fast8_t len);
 };
 
--- src/spi.cpp
+++ src/spi.cpp
@@ -33,7 +33,27 @@ SPI::SPI(uint32_t clk_frequency, uint_fast8_t mode){
   cout << "\tCPHA = " << clock_phase << endl;
   cout << "\tCPOL = " << clock_polarity << endl;
   cout << "\tSpeed = " << clock_frequency << "Hz" << endl << endl;
+
+  if(!bcm2835_init()){
+    cout << "BCM2835 init failed. Are you running as root?" << endl;
+  } else{
+    if(!bcm2835_spi_begin()){
+      cout << "BCM2835 spi init failed. Are you running as root?" << endl;
+    } else{
+      bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+      bcm2835_spi_setDataMode(mode);
+      bcm2835_spi_setClockDivider((int)(250000000 / clk_frequency) & ~0x01); // nearest even number
+      bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+      bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
+    }
+  }
 }
+
+SPI::~SPI(void){
+  bcm2835_spi_end();
+  bcm2835_close();
+}
+
 
 uint_fast8_t SPI::WriteRead(uint8_t *tx_data, uint8_t *rx_data, uint_fast8_t len){
   uint_fast8_t i;
@@ -45,6 +65,9 @@ uint_fast8_t SPI::WriteRead(uint8_t *tx_data, uint8_t *rx_data, uint_fast8_t len
     //*rx_data++ = i;
   }
   cout << endl;
+
+  tx_data -= len;
+  bcm2835_spi_transfernb((char *)&tx_data, (char *)&rx_data, len);
 
   return 0;
 }
